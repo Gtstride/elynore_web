d96cd3591e8f026dac1f298792e0c165332243eb
olubusolami <olubusolao3@gmail.com>
2021-11-17 14:10:45 +0100

    fix get initial route

--- backend/index.js
+++ backend/index.js
@@ -1,70 +1,72 @@
-const dotenv = require("dotenv");
+/** @format */
+
+const dotenv = require('dotenv');
 dotenv.config();
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const connection = require("../backend/db");
-const auth = require("./middleware/authetication");
+const cors = require('cors');
+const connection = require('../backend/db');
+const auth = require('./middleware/authetication');
 const {
-  registerHandler,
-  contactHandler,
-} = require("./middleware/errorHandler");
+	registerHandler,
+	contactHandler,
+} = require('./middleware/errorHandler');
 
 //import routes
-const postRoute = require("./routes/posts");
-const contactRoute = require("./routes/contact");
+const postRoute = require('./routes/posts');
+const contactRoute = require('./routes/contact');
 
 (async function db() {
-  await connection();
+	await connection();
 })();
 
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, PATCH, DELETE'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Authorization'
+	);
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
 });
 
 //middleware
 app.use(express.json());
 
 //route middleware
-app.use("/", postRoute);
-app.use("/contact", contactHandler, contactRoute);
+app.use(postRoute);
+app.use('/contact', contactHandler, contactRoute);
 
-app.get("/contacts", auth, (req, res) => {
-  Contact.find()
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
+app.get('/contacts', auth, (req, res) => {
+	Contact.find()
+		.then((users) => {
+			res.send(users);
+		})
+		.catch((error) => {
+			res.status(400).send(error);
+		});
 });
 
 //delete one contact
-app.delete("/contacts/:id", auth, function (req, res) {
-  Contact.findByIdAndDelete(req.params.id)
-    .exec()
-    .then((result) => {
-      if (!result) {
-        return res.status(404).end();
-      }
-      return res
-        .status(200)
-        .json({ status: "Success", message: "Deleted Successfully" });
-    })
-    .catch((error) => next(err));
+app.delete('/contacts/:id', auth, function (req, res) {
+	Contact.findByIdAndDelete(req.params.id)
+		.exec()
+		.then((result) => {
+			if (!result) {
+				return res.status(404).end();
+			}
+			return res
+				.status(200)
+				.json({ status: 'Success', message: 'Deleted Successfully' });
+		})
+		.catch((error) => next(err));
 });
 app.listen(5000, function () {
-  console.log("App listening on port 5000!");
+	console.log('App listening on port 5000!');
 });
