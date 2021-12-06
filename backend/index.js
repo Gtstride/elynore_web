60a825d78c0d3f5c4258e1aa825fa734c65c11f1
olubusolami <olubusolao3@gmail.com>
2021-12-06 13:34:10 +0100

    modify path

--- backend/index.js
+++ backend/index.js
@@ -23,5 +23,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, async () => {
 	console.log('App listening on port 5000!');
 	await sequelize.authenticate();
-	console.log('Database Connected successfully!');
+	console.log('Database is Connected successfully!');
 });
