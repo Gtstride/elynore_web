e6436b5466bb956141b40c3fd31f2c326f7c85d3
olubusolami <olubusolao3@gmail.com>
2021-12-08 14:58:26 +0100

    modify model index

--- backend/models/index.js
+++ backend/models/index.js
@@ -18,12 +18,7 @@ if (config.use_env_variable) {
 		config.database,
 		config.username,
 		config.password,
-		config,
-		{
-			dialectOptions: {
-				ssl: { rejectUnauthorized: false },
-			},
-		}
+		config
 	);
 }
 
